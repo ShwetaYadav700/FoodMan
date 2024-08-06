9db0a347b9e3985eca65ca4d055b915bed60c4b9
Shweta yadav <137385224+ShwetaYadav700@users.noreply.github.com>
2024-08-06 21:06:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+const menuBtn = document.getElementById('menu_btn');
+const navLinks = document.getElementById('nav_links');
+const menuBtnIcon = document.querySelector('i');
+
+menuBtn.addEventListener('click',(e)=>{
+    navLinks.classList.toggle("open");
+    const isOpen = navLinks.classList.contains("open");
+    menuBtnIcon.setAttribute('class',isOpen ? "ri-close-line":"ri-menu-line");
+})
+
+navLinks.addEventListener('click',(e)=>{
+    navLinks.classList.remove("open");
+    menuBtnIcon.setAttribute('class',"ri-menu-line");
+})
+
+document.addEventListener('DOMContentLoaded', function(){
+    const slides = document.querySelectorAll('.client_slide');
+    const bullets = document.querySelectorAll('.client_bullet');
+
+    let currentSlide = 0;
+    function showSlide(index){
+        const slidesContainer = document.querySelector('.client_slides');
+        slidesContainer.style.transform = `translate(-${index * 100}%)`;
+
+        bullets.forEach(bullet => bullet.classList.remove('active'));
+        bullets[index].classList.add('active');
+    }
+    bullets.forEach((bullet,index)=>{
+        bullet.addEventListener('click',()=>{
+            currentSlide = index;
+            showSlide(currentSlide);
+        });
+        showSlide(currentSlide);
+    })
+})
